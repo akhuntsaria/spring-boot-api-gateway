ab8dd438f84f0589a25cdf31c91682167bc973c2
akhuntsaria <akakikhuntsaria97@gmail.com>
2020-07-09 23:55:40 +0400

    Add sample service to protect it with gateway

---
+++ protected-service/src/main/java/com/github/akhuntsaria/protectedservice/ProtectedServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.akhuntsaria.protectedservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtectedServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtectedServiceApplication.class, args);
+    }
+
+}
---
+++ protected-service/src/main/java/com/github/akhuntsaria/protectedservice/controller/ProtectedController.java
@@ -0,0 +1,49 @@
+package com.github.akhuntsaria.protectedservice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/api/v1/protected")
+public class ProtectedController {
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseEntity<String> getSomeSensitiveData(HttpServletRequest request) {
+        String linkWithSensitiveData = "https://youtu.be/s35rVw1zskA";
+        String responseBody;
+
+        // display link nicely in browser
+        if (isRequestFromBrowser(request)) {
+            responseBody = wrapLinkInHtmlTag(linkWithSensitiveData);
+        } else {
+            responseBody = linkWithSensitiveData;
+        }
+
+        return new ResponseEntity<>(responseBody, HttpStatus.OK);
+    }
+
+    // check if 'Accept' header in request contains passed content type
+    private boolean acceptHeaderContainsContentType(HttpServletRequest request, String contentType) {
+        String acceptHeader = request.getHeader("Accept");
+
+        if (acceptHeader == null) {
+            return false;
+        }
+
+        return acceptHeader.contains(contentType);
+    }
+
+    private boolean isRequestFromBrowser(HttpServletRequest request) {
+        return acceptHeaderContainsContentType(request, MediaType.TEXT_HTML.toString());
+    }
+
+    private String wrapLinkInHtmlTag(String link) {
+        return String.format("<a href='%s'>%s</a>", link, link);
+    }
+}
