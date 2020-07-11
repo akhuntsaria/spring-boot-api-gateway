9f9dedc8a0a6dee5129ae0c7fa877760456b75e8
akhuntsaria <akakikhuntsaria97@gmail.com>
2020-07-11 23:13:01 +0400

    Add discovery server

---
+++ discovery-server/src/main/java/com/github/akhuntsaria/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package com.github.akhuntsaria.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
